47995016604ff2c5cf2d46feb4675b6ac50e8e2e
Lúcio Pimenta <luciosystem1@hotmail.com>
2021-01-29 21:23:41 -0300

    Criando estrutura de projeto do lucio

---
+++ lucio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lucio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
