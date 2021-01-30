7e5f182d8dd8fb36189dfccac867d5ec0c4be72f
axellbrendow <axellbrendow@ioasys.com.br>
2021-01-30 17:43:51 -0300

    Add non generic approach example

--- axell/Application/Program.cs
+++ axell/Application/Program.cs
@@ -24,9 +24,27 @@ namespace Application
             Console.WriteLine();
         }
 
+        static void NonGenerics()
+        {
+            Console.WriteLine("NonGenerics");
+            var customers = new List<Customer>();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                customers.Add(new Customer(i, $"Customer{i}", "9999 9999 9999 9999"));
+            }
+
+            customers.ForEach(NonGenericBaseService.Save);
+
+            var customerList = NonGenericBaseService.List();
+            customerList.ForEach(customer => Console.WriteLine(((Customer)customer).Details()));
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
             Generics();
+            NonGenerics();
         }
     }
 }
