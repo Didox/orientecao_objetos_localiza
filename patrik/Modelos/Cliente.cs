92b340b78408f043f2031143c27e7a15d64937cc
Patrik Oliveira <patrikro.oliveira@gmail.com>
2021-01-30 10:41:28 -0300

    feat(Cliente): Implementando herança de pessoa para cliente

--- patrik/Modelos/Cliente.cs
+++ patrik/Modelos/Cliente.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using Patrik.Modelos;
 namespace Patrik.Modelos
 {
-    class Cliente
+    class Cliente : Pessoa
     {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-
+        public Cliente(int id, string genero, string nome, string telefone): base(id, genero, nome)
+        {
+            Telefone = telefone;   
+        }
+        public string Telefone { get; set; }
         private static List<Cliente> ListaDeClientes = new List<Cliente>();
 
         public void Salvar()
@@ -17,6 +20,14 @@ namespace Patrik.Modelos
         {
           return ListaDeClientes;
         }
+
+        public override string imprimePessoa()
+        {
+            var stringPessoa = base.imprimePessoa();
+            return $"{stringPessoa}, {Telefone}";
+        }
+
+
 
         public override string ToString()
         {
