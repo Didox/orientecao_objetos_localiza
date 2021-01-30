60b3008c6b32232bf29f504bf221289b69547545
Michele Freitas <micheler720@gmail.com>
2021-01-29 22:48:02 -0300

    Inclusao da classe Cliente, atributos, metodos conforme descrição da aula

--- michele/Program.cs
+++ michele/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using Michele.Models;
 
-namespace michele
+namespace Michele
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new Client();
+            client.Address= "Romoaldo Lopes Cançado, 224";
+            client.BirthDay = new DateTime(1996,05,06);
+            client.Name = "Michele de Freitas Ribeiro";
+            client.Id = 123456;
+            var clientSearch = Client.SearchName("Michele de Freitas Ribeiro");
+            Client.Save();
+            Console.WriteLine(client);
         }
     }
 }
