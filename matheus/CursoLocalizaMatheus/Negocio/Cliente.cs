de4ce6eb484c01c48de6cf422c4eb5a9dd3be92b
Matheus Sinnis <matheussinnis@hotmail.com>
2021-01-30 18:43:28 -0300

    Generics, partials e seleada

---
+++ matheus/CursoLocalizaMatheus/Negocio/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Negocio
+{
+    public partial class Cliente
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+}
--- matheus/CursoLocalizaMatheus/UIConsole/Program.cs
+++ matheus/CursoLocalizaMatheus/UIConsole/Program.cs
@@ -11,6 +11,13 @@ namespace UIConsole
             carro.Ano = 2020;
             carro.Nome = "Palio";
             CarroService.Salvar(carro);
+
+            //Teste Partial Classe
+            Cliente.Especial();
+
+            //Generics
+            var listaCarros = NegocioService.Todos<Carro>();
+
         }
     }
 }
