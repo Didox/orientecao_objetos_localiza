bb40f67bee109b64de607417b733965ed23089f4
Cássio Morais <cassiomoraisg@hotmail.com>
2021-01-30 18:41:00 -0300

    fix colunas

--- cassio/Database/BDService.cs
+++ cassio/Database/BDService.cs
@@ -5,7 +5,7 @@ namespace Database
 {
     public class BDService
     {
-        ICollection<string> colunas = new HashSet<string>();
+       
 
         public virtual void Salvar<T>(T generico)
         {
@@ -13,6 +13,7 @@ namespace Database
             var campos = tipo.GetProperties();
 
             string query = $"insert into {generico.GetType().Name} values (";
+            ICollection<string> colunas = new HashSet<string>();
 
             foreach (var campo in campos)
             {
