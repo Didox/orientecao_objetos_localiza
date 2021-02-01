f56c2edd46e44bda169b65ee886cbc29d3d1e00f
vianaerica <erica.viana01@gmail.com>
2021-02-01 15:36:51 -0300

    Sincronizando pasta do danilo

---
+++ danilo/TresCamadasLocaliza/TresCamadasLocaliza/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Database;
+
+namespace Negocio
+{
+    [Tabela(Nome = "tbl_carros")]
+    public class Carro : Dado
+    {
+        [CampoPersistido(NomeColuna = "nome_carro")]
+        public string Nome { get; set; }
+
+        [CampoPersistido]
+        public string Telefone { get; set; }
+
+        public int Ano { get; set; }
+
+        public string Descricao { get; set; }
+
+        public void Salvar()
+        {
+            CarroDbService.Salvar(this);
+        }
+    }
+}
---
+++ danilo/TresCamadasLocaliza/UIWeb/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Negocio;
+using UIWeb.Models;
+
+namespace UIWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            new Carro()
+            {
+                Id = 1,
+                Nome = "Danilo",
+                Telefone = "12222",
+                Ano = 2021,
+                Descricao = "Um teste"
+            }.Salvar();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
