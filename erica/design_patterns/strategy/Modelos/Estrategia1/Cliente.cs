35a03bf17301110d0622f5f7ccd05a0821aef3a1
vianaerica <erica.viana01@gmail.com>
2021-02-01 19:12:29 -0300

    Criação de estrutura de pastas strategy  e criação de Cliente

---
+++ erica/design_patterns/strategy/Modelos/Estrategia1/Cliente.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace strategy.Modelos.Estrategia1
+{
+    public class Fisica
+    {
+        public int id {get;set;}
+        public string nome {get;set;}
+        public string telefone {get;set}
+        public string email {get; set;}
+    }
+}
