4f0744601d6f8060c0349082c31cb613dce2dc5f
vianaerica <erica.viana01@gmail.com>
2021-02-02 09:44:41 -0300

    Abrindo instancia p/ serviços e fechando após o uso

--- erica/design_patterns/singleton/Program.cs
+++ erica/design_patterns/singleton/Program.cs
@@ -6,6 +6,21 @@ namespace singleton
     {
         static void Main(string[] args)
         {
+            EntidadeUseCase.ListaAcoes.Add(EmailService.Instancia());
+            EntidadeUseCase.ListaAcoes.Add(SmsService.Instancia());
+            EntidadeUseCase.ListaAcoes.Add(DbService.Instancia());
+            
+            //EntidadeUseCase.ListaAcoes.Add(MongoService.Instancia());
+            //EntidadeUseCase.ListaAcoes.Add(ElascService.Instancia());
+
+            EntidadeUseCase.Salvar(new Cliente(){
+                Nome = "Erica",
+                Telefone = "12345"
+            });
+
+            DbService.Instancia().Fechar();
+            SmsService.Instancia().Fechar();
+            EmailService.Instancia().Fechar();
             //Console.WriteLine("Hello World!");
         }
     }
