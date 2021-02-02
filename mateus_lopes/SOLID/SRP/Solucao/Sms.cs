5a8ef55ed70ca44a24456e00cffd0b0f67107777
Mateus Lopes <mateusfellipe2302@gmail.com>
2021-02-02 20:03:52 -0300

    Adicionado exemplo single responsibility principle do SOLID

---
+++ mateus_lopes/SOLID/SRP/Solucao/Sms.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace mateus_lopes.SOLID.SRP.Solucao
+{
+    public class Sms
+    {
+        public void EnviaSms(Carro carro)
+        {
+            Console.WriteLine("Enviando Sms");
+        }
+    }
+}
