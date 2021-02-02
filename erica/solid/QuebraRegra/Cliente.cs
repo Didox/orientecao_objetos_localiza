108237ad7dbabc5213eb048a090773b4f47ff130
vianaerica <erica.viana01@gmail.com>
2021-02-02 19:52:08 -0300

    Criação de pasta e projeto para atividade SOLID. 1- SRP

---
+++ erica/solid/QuebraRegra/Cliente.cs
@@ -0,0 +1,10 @@
+namespace solid.QuebraRegra
+{
+    public class Cliente
+    {
+        public int id {get;set;}
+        public string nome {get;set;}
+        public string email {get;set;}
+        public string celular {get;set;}
+    }
+}
---
+++ erica/solid/SRP/Sms.cs
@@ -0,0 +1,7 @@
+namespace solid.SRP
+{
+    public class Sms
+    {
+        
+    }
+}
