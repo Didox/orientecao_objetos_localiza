2c4f23f8354205ff01b3683bad5463e54d2e0848
vianaerica <erica.viana01@gmail.com>
2021-02-02 09:41:51 -0300

    Implementando método de Salvar em EntidadeUseCase, para enviar ao salvar

--- erica/design_patterns/singleton/Dominio/UseCase/EntidadeUseCase.cs
+++ erica/design_patterns/singleton/Dominio/UseCase/EntidadeUseCase.cs
@@ -2,5 +2,11 @@ namespace singleton.Dominio.UseCase
 {
     public class EntidadeUseCase
     {
+      public static List<IService> ListaAcoes = new List<IService>();
+      public static void Salvar<T>(T entidade){
+        foreach(var acao in ListaAcoes){
+          acao.Enviar(entidade);
+        }
+      }
     }
 }
