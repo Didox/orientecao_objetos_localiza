c9fb934f35dd7d7ff4ace980a97097c754d78e64
Cássio Morais <cassiomoraisg@hotmail.com>
2021-02-02 20:08:57 -0300

    fix: interface IAula implementação

--- cassio/SOLID/SRP/SRP/SRP/NaRegra/Repository/AulaRepository.cs
+++ cassio/SOLID/SRP/SRP/SRP/NaRegra/Repository/AulaRepository.cs
@@ -6,8 +6,8 @@ using System.Text;
 namespace SRP.NaRegra.Repository
 {
     public class AulaRepository : IRepository
-    {
-        public void Salvar(Aula aula)
+    { 
+        public void Salvar(IAula aula)
         {
             Console.WriteLine("Salvando Aula...");
         }
