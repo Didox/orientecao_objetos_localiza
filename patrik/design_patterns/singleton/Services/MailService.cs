fa200cb281385390ba93514e6fa21a0afaae91ab
Patrik Oliveira <patrikro.oliveira@gmail.com>
2021-02-01 21:17:37 -0300

    feat(patterns): Implementando pattern singleton

---
+++ patrik/design_patterns/singleton/Services/MailService.cs
@@ -0,0 +1,32 @@
+using System;
+using singleton.Services;
+
+namespace singleton.Modelos
+{
+    public class MailService
+    {
+        private MailService() {}
+
+        private static MailService _mailService;
+
+        public void Enviar<T>(T obj)
+        {
+            Console.WriteLine($"Enviando SMS do tipo {obj.GetType().Name}");
+        }
+
+        public void Fechar()
+        {
+            _mailService = null;
+        }
+        
+        public static MailService Instancia()
+        {
+            if (_mailService == null)
+            {
+                _mailService = new MailService();
+            }
+
+            return _mailService;
+        }
+    }
+}
