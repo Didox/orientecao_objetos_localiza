a4cc88b0372a4b3b99b446296b16eb854afa8950
Cássio Morais <cassiomoraisg@hotmail.com>
2021-02-02 20:06:12 -0300

    Single Responsiblity Principle

---
+++ cassio/SOLID/SRP/SRP/SRP/NaRegra/Models/IAula.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRP.NaRegra.Models
+{
+    public interface IAula
+    {
+        public int Id { get; set; }
+    }
+}
