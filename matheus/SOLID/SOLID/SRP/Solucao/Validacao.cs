fcd5878d2517ab4272c7c4882a3066052ffb48f4
Matheus Sinnis <matheussinnis@hotmail.com>
2021-02-02 20:00:53 -0300

    Exercicio Solid SRP

---
+++ matheus/SOLID/SOLID/SRP/Solucao/Validacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID.SRP.Solucao
+{
+    public class Validacao
+    {
+
+        public static bool Validar()
+        {
+            //Valida Cliente
+            return true;
+        }
+    }
+}
