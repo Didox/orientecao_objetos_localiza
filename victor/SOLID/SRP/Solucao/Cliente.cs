eb6d2136470b275d29e5e107db8a3c0fb9f1e872
vstorresti <vstorresti@gmail.com>
2021-02-02 20:03:51 -0300

    Adicionando princípio SOLID (SRP) e Design Patterns (Builder / Factory)

---
+++ victor/SOLID/SRP/Solucao/Cliente.cs
@@ -0,0 +1,15 @@
+namespace SOLID.SRP.Solucao
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Cpf { get; set; }
+
+        public Cliente()
+        {
+        }
+    }
+}
