e063f5a34c3a979e9f908a61fc7e55022e13ebc7
vianaerica <erica.viana01@gmail.com>
2021-02-01 21:20:12 -0300

    Adicionando metodos p/ salvar de acordo com o tipo do Cliente

--- erica/design_patterns/strategy/Modelos/Estrategia1/Cliente.cs
+++ erica/design_patterns/strategy/Modelos/Estrategia1/Cliente.cs
@@ -10,9 +10,34 @@ namespace strategy.Modelos.Estrategia1
         public string email {get; set;}
         public TipoCliente tipoCliente {get;set;}
 
-        public List<Cliente> Salvar(){
-            Console.WriteLine("Salvando..");
-            return new List<Cliente>();
-        }
+         public void Salvar()
+    {
+      if(this.Tipo == TipoCliente.Fisica)
+      {
+        Console.WriteLine("Salvo como pessoa Fisica");
+      }
+      else if(this.Tipo == TipoCliente.Jurifica)
+      {
+        Console.WriteLine("Salvando como pessoa Juridica");
+      }
+    }
+
+    public static List<Cliente> Todos(TipoCliente? tipo = null)
+    {
+      if(tipo == null) return new List<Cliente>();
+
+      if(tipo == TipoCliente.Fisica)
+      {
+        return new List<Cliente>();
+      }
+      else if(tipo == TipoCliente.Jurifica)
+      {
+        return new List<Cliente>();
+      }
+      else
+      {
+        return new List<Cliente>();
+      }
+    }
     }
 }
