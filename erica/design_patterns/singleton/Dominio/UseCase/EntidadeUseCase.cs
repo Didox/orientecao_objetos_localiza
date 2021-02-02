97d20266aa49149868cf225ea2c25f14dfdebe6a
vianaerica <erica.viana01@gmail.com>
2021-02-02 09:31:12 -0300

    Implementando métodos de instância para a classe DBServices

--- erica/design_patterns/singleton/Dominio/UseCase/EntidadeUseCase.cs
+++ erica/design_patterns/singleton/Dominio/UseCase/EntidadeUseCase.cs
@@ -2,8 +2,5 @@ namespace singleton.Dominio.UseCase
 {
     public class EntidadeUseCase
     {
-    public string nome {get;set;}
-    public string telefone {get;set;}
-    public string email {get;set;}
     }
 }
