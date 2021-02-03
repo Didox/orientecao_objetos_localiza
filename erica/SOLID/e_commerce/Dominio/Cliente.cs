56d5e2eab0458d40c133ad8c1667c2e84a870885
vianaerica <erica.viana01@gmail.com>
2021-02-03 17:34:12 -0300

    Adicionando atributos para cliente

--- erica/SOLID/e_commerce/Dominio/Cliente.cs
+++ erica/SOLID/e_commerce/Dominio/Cliente.cs
@@ -2,6 +2,9 @@ namespace e_commerce
 {
     public class Cliente
     {
-        
+        public int id {get;set;}
+        public string nome {get;set;}
+        public string telefone {get;set;}
+        public string email {get;set;}
     }
 }
