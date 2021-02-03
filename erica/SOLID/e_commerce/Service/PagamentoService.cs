a1b9c6d91c11ef77988f671dc0c68eeaa74f4d27
vianaerica <erica.viana01@gmail.com>
2021-02-03 18:24:33 -0300

    Implementando serviço de pagamento e serviço p/ SMS

--- erica/SOLID/e_commerce/Service/PagamentoService.cs
+++ erica/SOLID/e_commerce/Service/PagamentoService.cs
@@ -1,7 +1,24 @@
 namespace e_commerce
 {
-    public class Pagamento : IService
+    public class PagamentoService : IService
     {
-        
+          private PagamentoService(){}
+
+    #region instancia
+        public void Enviar<T>(T obj){
+            Console.WriteLine($"Pagamento do tipo {obj.GetType().Name}");
+        }
+        public void Fechar(){
+            entidadePagamentoService = null;
+        }
+    #endregion
+
+    private static PagamentoService entidadePagamentoService;
+    private static object entidadeDbService;
+
+    public static PagamentoService instancia(){
+      if(entidadePagamentoService == null) entidadePagamentoService = new PagamentoService();
+      return entidadePagamentoService;
+      }
     }
 }
