5d2fe227b2bd781d2d5e939c39eaf8de3eb4f1d6
Matheus Sinnis <matheussinnis@hotmail.com>
2021-02-03 13:47:49 -0300

    Exercicio Produtos

---
+++ matheus/ExercicioProdutos/DataBase/DBServiceGeneric.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBase
+{
+    public class DBServiceGeneric
+    {
+
+        public static bool Salvar<T>(T dado)
+        {
+            try
+            {
+                //Salvar objeto no banco de dados
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+    
+        }
+
+    }
+}
---
+++ matheus/ExercicioProdutos/Negocio/EmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Negocio
+{
+    public class EmailService
+    {
+        public static bool Enviar<T>(T dado)
+        {
+            //Envia Email
+            return true;
+        }
+    }
+}
