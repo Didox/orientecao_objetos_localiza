85956501184679b91e196fa19b423aba5d943e89
vianaerica <erica.viana01@gmail.com>
2021-02-04 22:13:21 -0300

    Alterando classe de Customer conforme o Dapper Contrib

--- erica/DapperDB/Domain/Entities/Customer.cs
+++ erica/DapperDB/Domain/Entities/Customer.cs
@@ -5,13 +5,10 @@ namespace DapperDB.Domain.Entities
     {
         [Key]
         public int id { get; set; }
-
-        [Fields(ColumnName = "name")]
         public string name { get; set; }
-        [Fields(ColumnName = "email")]
         public string email { get; set; }
-        [Fields(ColumnName = "phone")]
         public string phone { get; set; }
+        [ExplicitKey]
         public CustomerType CustomerType {get;set;}
     }
 }
