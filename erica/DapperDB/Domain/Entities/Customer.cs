7ce2f6e7b31ab596c23fa435dfd456ec89104edd
vianaerica <erica.viana01@gmail.com>
2021-02-04 21:02:22 -0300

    Criação de estrutura de pastas e classe Customer

---
+++ erica/DapperDB/Domain/Entities/Customer.cs
@@ -0,0 +1,11 @@
+namespace DapperDB.Domain.Entities
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int  idade { get; set; }
+        public string email { get; set; }
+        public string telefone { get; set; }
+    }
+}
