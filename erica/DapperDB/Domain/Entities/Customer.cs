7255d438c077718d608e28e453c43b199e2d523c
vianaerica <erica.viana01@gmail.com>
2021-02-04 21:11:09 -0300

    Adicionando atributos na classe Customer

--- erica/DapperDB/Domain/Entities/Customer.cs
+++ erica/DapperDB/Domain/Entities/Customer.cs
@@ -2,10 +2,14 @@ namespace DapperDB.Domain.Entities
 {
     public class Customer
     {
+        [Pk(Name="id")]
         public int id { get; set; }
-        public string nome { get; set; }
-        public int  idade { get; set; }
+
+        [Fields(ColumnName = "name")]
+        public string name { get; set; }
+        [Fields(ColumnName = "email")]
         public string email { get; set; }
-        public string telefone { get; set; }
+        [Fields(ColumnName = "phone")]
+        public string phone { get; set; }
     }
 }
--- erica/DapperDB/Program.cs
+++ erica/DapperDB/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Dapper;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace DapperDB
 {
@@ -6,7 +9,32 @@ namespace DapperDB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+            string _con = Configuration["ConnectionStrings:DefaultConnection"];
+            Console.WriteLine("\tc - Consultar");
+            Console.WriteLine("\ti - Incluir");
+            Console.WriteLine("\ta - Atualizar");
+            Console.WriteLine("\td - Deletar");
+            Console.Write("Sua opção (c,i,d,a) ? ");
+            switch (Console.ReadLine())
+            {
+                case "c":
+                    Consultar(_con);
+                    break;
+                case "i":
+                    Incluir(_con);
+                    break;
+                case "a":
+                    Atualizar(_con);
+                    break;
+                case "d":
+                    Deletar(_con);
+                    break;
+            }
+            Console.ReadKey();*/
         }
     }
 }
