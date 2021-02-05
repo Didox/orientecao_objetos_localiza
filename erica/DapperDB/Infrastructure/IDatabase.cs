d4a4ab5b398fa1d76e05a3b85e5c61e49f364a88
vianaerica <erica.viana01@gmail.com>
2021-02-04 21:44:55 -0300

    Implementando save, listAll e remove na interface de Database

--- erica/DapperDB/Infrastructure/IDatabase.cs
+++ erica/DapperDB/Infrastructure/IDatabase.cs
@@ -1,7 +1,9 @@
 namespace DapperDB.Infrastructure
 {
-    public interface IDatabase
+    public interface IDatabase<T> where T : class
     {
-        
+        void Save(T obj);
+        List<T> All(string sqlWhere = null);
+        void Remove(T obj);
     }
 }
