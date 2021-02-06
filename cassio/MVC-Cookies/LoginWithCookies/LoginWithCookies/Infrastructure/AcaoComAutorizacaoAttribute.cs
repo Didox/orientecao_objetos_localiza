2a5d521fadac0d16ef80d9e32bdc3c21f1f554fb
Cássio Morais <cassiomoraisg@hotmail.com>
2021-02-06 11:26:18 -0300

    Autenticação simples com Cookies

---
+++ cassio/MVC-Cookies/LoginWithCookies/LoginWithCookies/Infrastructure/AcaoComAutorizacaoAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LoginWithCookies.Controllers
+{
+    internal class AcaoComAutorizacaoAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (string.IsNullOrEmpty(filterContext.HttpContext.Request.Cookies["Cassio"]))
+            {
+                filterContext.HttpContext.Response.Redirect("/Home/Login");
+                return;
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ cassio/MVC-Cookies/LoginWithCookies/LoginWithCookies/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginWithCookies.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Usuário é requerido!")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Senha é requerida!")]
+        public string Senha { get; set; }
+    }
+}
