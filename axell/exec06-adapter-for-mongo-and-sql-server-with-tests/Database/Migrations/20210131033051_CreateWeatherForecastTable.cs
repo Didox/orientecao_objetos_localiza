984ddf299c40bfd5782272d47745d7ed42c3f355
axellbrendow <axellbrendow@ioasys.com.br>
2021-02-05 23:36:29 -0300

    Add migrations for WeatherForecast model

---
+++ axell/exec06-adapter-for-mongo-and-sql-server-with-tests/Database/Migrations/20210131033051_CreateWeatherForecastTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetCoreApi.Database.Migrations
+{
+    public partial class CreateWeatherForecastTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherForecasts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TemperatureC = table.Column<int>(type: "int", nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherForecasts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherForecasts");
+        }
+    }
+}
