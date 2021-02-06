75ab128a8c76f371aa5c3aad6b6425684c28a068
vianaerica <erica.viana01@gmail.com>
2021-02-06 09:51:19 -0300

    Sincronização da pasta do danilo. Criação do projeto cookies

---
+++ erica/cookies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cookies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
